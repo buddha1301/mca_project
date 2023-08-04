0124ef82b596274c2564db5bcdda4b5926d4007a
buddha1301 <137280312+buddha1301@users.noreply.github.com>
2023-08-04 08:18:04 +0000

    added age input

--- app/admin/page.js
+++ app/admin/page.js
@@ -10,6 +10,7 @@ function Page() {
     const [vote,setVote] = useState("")
     const [gender,setGender] = useState("")
     const [name,setDisplayName] = useState("")
+    const [age,setAge] = useState("")
     const [ward,setWard] = useState("")
     const [dob,setDateOfBirth] = useState("")
     const [district,setDistrict] = useState("")
@@ -62,6 +63,13 @@ function Page() {
                       <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Voter Id Number</label>
                       <input type="text" name="phone_number" id="confirm-password"   onChange={(e)=>setVoterId(e.target.value)} value = {voter_id} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                   </div>
+
+                  <div>
+                      <label htmlFor="age" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Voter Id Number</label>
+                      <input type="text" name="age" id="age"   onChange={(e)=>setAge(e.target.value)} value = {age} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                  </div>
+
+
 
                   <div>
                       <label htmlFor="voter_id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date of Birth</label>
