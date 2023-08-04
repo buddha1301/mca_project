c698dd983d21c1523696801aefdd7269b45e89e1
buddha1301 <137280312+buddha1301@users.noreply.github.com>
2023-08-04 08:38:32 +0000

    added error check

--- app/(firebase)/addData.js
+++ app/(firebase)/addData.js
@@ -1,29 +1,30 @@
 import { getFirestore,doc,setDoc, projectFirestore } from "./config";
-const district_tables=["Gangtok","Mangan","Pakyong","Namchi","Soreng"]
+//const district_tables=["Gangtok","Mangan","Pakyong","Namchi","Soreng"]
 
 export default async function addData(collection,id,data)
 {
     let result = null; 
     let error = null;
-    console.log(data)
-    try {
-        for(x of district_tables)
-        {
-            const docRef = doc(db, x, data["voter_id"]);
-
-        // Get a document, forcing the SDK to fetch from the offline cache.
         try {
-            const doc = await getDocFromCache(docRef);
+             const docRef = doc(db, data['district'], data["voter_id"]);
+
+            // Get a document, forcing the SDK to fetch from the offline cache.
+            try {
+                 const doc = await getDocFromCache(docRef);
 
              // Document was found in the cache. If no cached document exists,
             // an error will be returned to the 'catch' block below.
-            error=doc
+                error=doc
             
-            }      
+            }     
          catch (e) {
                 
             console.log("Yay")
         }
+        }
+        catch(e)
+        {
+            error=e
         }
 
         
@@ -37,11 +38,6 @@ export default async function addData(collection,id,data)
             merge:true
         });
     }
-    }
-    catch(e)
-    {
-        error = e
+    return {result,error};
     }
 
-    return {result,error};
-}
