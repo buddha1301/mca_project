c75dc52f9501a19abb9a7dfafacab9b53845e9d0
buddha1301 <137280312+buddha1301@users.noreply.github.com>
2023-08-04 08:13:31 +0000

    added edge cases

--- app/(firebase)/addData.js
+++ app/(firebase)/addData.js
@@ -1,4 +1,5 @@
 import { getFirestore,doc,setDoc, projectFirestore } from "./config";
+const district_tables=["Gangtok","Mangan","Pakyong","Namchi","Soreng"]
 
 export default async function addData(collection,id,data)
 {
@@ -6,9 +7,36 @@ export default async function addData(collection,id,data)
     let error = null;
     console.log(data)
     try {
+        for(x of district_tables)
+        {
+            const docRef = doc(db, x, data["voter_id"]);
+
+        // Get a document, forcing the SDK to fetch from the offline cache.
+        try {
+            const doc = await getDocFromCache(docRef);
+
+             // Document was found in the cache. If no cached document exists,
+            // an error will be returned to the 'catch' block below.
+            error=doc
+            
+            }      
+         catch (e) {
+                
+            console.log("Yay")
+        }
+        }
+
+        
+         if (data['age']< 18)
+        {
+            error= "You are less than 18 years old "
+        }
+
+        else{
         result = await setDoc(doc(projectFirestore,collection,id),data,{
             merge:true
         });
+    }
     }
     catch(e)
     {
